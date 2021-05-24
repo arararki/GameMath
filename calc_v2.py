ebae421df1d51d10f6be59d3fe303317f06cfa93
Muhammad <maxkarimov077@gmail.com>
2021-05-24 20:02:48 +0300

    Ещё обноввляем новый функционал (Комиила Алина)

--- calc_v2.py
+++ calc_v2.py
@@ -33,5 +33,39 @@ def f21(x):
     if x[0] == 1985:
         return 12
 
+
+
+
+def f22(x):
+    temp_ = bin(x)
+    temp = (str(temp_[2:]))
+    if len(temp) == 29:
+        temp = '000' + temp
+    if len(temp) == 30:
+        temp = '00' + temp
+    if len(temp) == 31:
+        temp = '0' + temp
+    e_ = temp[0:1]
+    d_ = temp[1:7]
+    c_ = temp[7:11]
+    b_ = temp[11:24]
+    a_ = temp[24:32]
+    new_temp = '0b' + d_ + b_ + a_ + c_ + e_
+    new_x = int(new_temp,2)
+    s = hex(new_x)
+    s_ = int(s, 16)
+    return s_
+
+def trans(new_tabl,new_tabl_):
+    for i in range(len(new_tabl[0])):
+        row = []
+        for item in new_tabl:
+            row.append(item[i])
+        new_tabl_.append(row)
+    return new_tabl_
+
 x = [1972, 2004, 'ampl', 1992, 'r']
 f21(x)
+
+x = 0x3799ec28
+f22(x)
